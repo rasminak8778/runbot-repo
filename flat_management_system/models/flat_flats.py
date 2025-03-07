72a7c78e76e0d841f63c8c2fbff3226f8482ed38
rasminak8778 <165371733+rasminak8778@users.noreply.github.com>
2025-03-07 11:14:53 +0530

    Add files via upload

---
+++ flat_management_system/models/flat_flats.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class FlatFlats(models.Model):
+    _name = 'flat.flats'
+    _description = 'Flats For Sale'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _rec_name = 'name'
+
+    name = fields.Char('namemme', required=True)
+    description = fields.Char(string='Description', help='Description for the flat')
+    amount = fields.Float(string='Amount')
+    flat_id = fields.Many2one('flat.management', string="Flat")
+    
+    sum= fields.Float(string='Sum')
